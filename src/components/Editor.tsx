5823dd3075e381dbf50f66bc501770cd5369300c
Anser Waseem <hafiz.anser.waseem@gmail.com>
2022-11-06 23:57:08 +0500

    fixed some edge cases in transition table and added skeleton for automata toolbar

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
-import { Box, Button, Grid, Paper, styled } from "@mui/material";
 import {
-  DataGrid,
-  GridColDef,
-  GridRenderCellParams,
-  GridRowProps,
-  GridColumns,
-  GridActionsCellItem,
-} from "@mui/x-data-grid";
+  Box,
+  Button,
+  Grid,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Slide,
+  Tooltip,
+} from "@mui/material";
+import { DataGrid, GridColumns, GridActionsCellItem } from "@mui/x-data-grid";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditSharpIcon from "@mui/icons-material/EditSharp";
 import SaveSharpIcon from "@mui/icons-material/SaveSharp";
@@ -17,16 +20,6 @@ import Row from "../models/TransitionTableRow.model";
 
 const getBackgroundColor = (color: string, mode: string) =>
   mode === "dark" ? darken(color, 0.6) : lighten(color, 0.6);
-const getHoverBackgroundColor = (color: string, mode: string) =>
-  mode === "dark" ? darken(color, 0.5) : lighten(color, 0.5);
-
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(2),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-}));
 
 const Editor = () => {
   const [gridRowId, setGridRowId] = useState(1);
@@ -46,6 +39,7 @@ const Editor = () => {
       editable: true,
       disableColumnMenu: true,
       sortable: false,
+      width: 50,
     },
     {
       field: "b",
@@ -53,6 +47,7 @@ const Editor = () => {
       editable: true,
       disableColumnMenu: true,
       sortable: false,
+      width: 50,
     },
     {
       field: "nul",
@@ -60,13 +55,13 @@ const Editor = () => {
       editable: true,
       disableColumnMenu: true,
       sortable: false,
+      width: 50,
     },
     {
       field: "action",
       headerName: "Action",
       disableColumnMenu: true,
       sortable: false,
-      width: 200,
       type: "actions",
       getActions: (params) => {
         return [
@@ -86,19 +81,24 @@ const Editor = () => {
           <GridActionsCellItem
             icon={<SecurityIcon />}
             label="Toggle initial State"
-            onClick={() => toggleInitialState(params.row.id)}
+            onClick={() => toggleInitialState(params.row)}
             showInMenu
           />,
           <GridActionsCellItem
             icon={<SecurityIcon />}
             label="Toggle Final State"
-            onClick={() => toggleFinalState(params.row.id)}
+            onClick={() => toggleFinalState(params.row)}
             showInMenu
           />,
         ];
       },
     },
   ];
+  const [selectedIndex, setSelectedIndex] = useState(1);
+
+  function handleListItemClick(event: any, index: number) {
+    setSelectedIndex(index);
+  }
 
   const handleAddRow = () => {
     setGridData((prev) => [
@@ -108,36 +108,53 @@ const Editor = () => {
     setGridRowId((prev) => prev + 1);
   };
 
-  //   const handleEditRow = (row: Row) => {
-  //     setGridRow(row);
-  //   };
-
   const handleDeleteRow = (id: number) => {
     console.log("handleDeleteRow", id);
     setGridData((prev) => prev.filter((row) => row.id !== id));
   };
 
+  const isRowEmpty = (row: Row) => {
+    return row.node === "" && row.a === "" && row.b === "" && row.nul === "";
+  };
+
   const handleSaveRow = (row: Row) => {
     console.log("handleSaveRow", row);
     setGridData((prev) => {
+      console.log("handleSaveRow prev", prev);
+      if (!prev || isRowEmpty(row)) {
+        alert("Cannot save empty row.");
+        return prev;
+      }
+
+      const availableNodeValues = prev
+        .map((r) => r.node)
+        .filter((v) => v !== "");
+      if (!availableNodeValues.includes(row.node))
+        availableNodeValues.concat(row.node);
+
+      console.log("availableNodeValues", availableNodeValues);
+
+      const areTransitionValuesInvalid = [
+        row.a !== "" && !availableNodeValues.includes(row.a),
+        row.b !== "" && !availableNodeValues.includes(row.b),
+        row.nul !== "" && !availableNodeValues.includes(row.nul),
+      ];
+      console.log("areTransitionValuesInvalid", areTransitionValuesInvalid);
+      if (areTransitionValuesInvalid.includes(true)) {
+        alert(
+          `Transition values must be empty or one of the following: ${availableNodeValues.join(
+            ", "
+          )}`
+        );
+        return prev;
+      }
+
       const nodeAlreadyExists = prev.find(
         (r) => r.node === row.node && r.id !== row.id
       );
       if (nodeAlreadyExists) {
         alert("This node value already exists. Kindly choose another value.");
-        return [...prev];
-      }
-
-      const availableNodeValues = prev.map((r) => r.node);
-      if (
-        !availableNodeValues.includes(row.a) ||
-        !availableNodeValues.includes(row.b) ||
-        !availableNodeValues.includes(row.nul)
-      ) {
-        alert(
-          "This node value doesn't exists. Kindly choose one of the existing node value."
-        );
-        return [...prev];
+        return prev;
       }
 
       let newGridData = [...prev];
@@ -148,22 +165,39 @@ const Editor = () => {
     });
   };
 
-  const toggleInitialState = (id: number) => {
-    console.log("toggleInitialState", id);
+  const toggleInitialState = (row: Row) => {
+    console.log("toggleInitialState", row);
     setGridData((prev) => {
+      console.log("toggleInitialState prev", prev);
+      if (!prev || isRowEmpty(row)) {
+        alert("Cannot make empty row initial state.");
+        return prev;
+      }
+
+      if (prev.filter((r) => r.isInitial).length > 0 && !row.isInitial) {
+        alert("Only one initial state is allowed.");
+        return prev;
+      }
+
       let newGridData = [...prev];
-      let index = newGridData.findIndex((r) => r.id === id);
+      let index = newGridData.findIndex((r) => r.id === row.id);
       newGridData[index].isInitial = !newGridData[index].isInitial;
       console.log("newGridData", newGridData);
       return newGridData;
     });
   };
 
-  const toggleFinalState = (id: number) => {
-    console.log("toggleFinalState", id);
+  const toggleFinalState = (row: Row) => {
+    console.log("toggleFinalState", row);
     setGridData((prev) => {
+      console.log("toggleFinalState prev", prev);
+      if (!prev || isRowEmpty(row)) {
+        alert("Cannot make empty row final state.");
+        return prev;
+      }
+
       let newGridData = [...prev];
-      let index = newGridData.findIndex((r) => r.id === id);
+      let index = newGridData.findIndex((r) => r.id === row.id);
       newGridData[index].isFinal = !newGridData[index].isFinal;
       console.log("newGridData", newGridData);
       return newGridData;
@@ -173,8 +207,10 @@ const Editor = () => {
   return (
     <>
       <Box sx={{ flexGrow: 1, m: 1 }}>
+        {/* Grid to incorporate Transition table and Automata */}
         <Grid container columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-          <Grid item xs={12} sm={4}>
+          {/* Transition table grid */}
+          <Grid item sm={12} md={4}>
             <Button size="small" onClick={handleAddRow}>
               Add a row
             </Button>
@@ -223,8 +259,78 @@ const Editor = () => {
               ></DataGrid>
             </Box>
           </Grid>
-          <Grid item xs={12} sm={8}>
-            <Item>2</Item>
+          {/* Automata grid */}
+          <Grid container item sm={12} md={8}>
+            {/* Automata toolbar grid */}
+            <Grid container item sm={12} md={2}>
+              <Slide direction="up" in mountOnEnter unmountOnExit>
+                <Grid
+                  container
+                  item
+                  direction={{ sm: "row", md: "column" }}
+                  justifyContent={{
+                    sm: "space-evenly",
+                  }}
+                >
+                  <Grid item>
+                    <ListItem
+                      button
+                      selected={selectedIndex === 0}
+                      onClick={(event) => handleListItemClick(event, 0)}
+                    >
+                      <Tooltip title="Add node">
+                        <ListItemIcon>
+                          <EditSharpIcon />
+                        </ListItemIcon>
+                      </Tooltip>
+                    </ListItem>
+                  </Grid>
+                  <Grid item>
+                    <ListItem
+                      button
+                      selected={selectedIndex === 1}
+                      onClick={(event) => handleListItemClick(event, 1)}
+                    >
+                      <Tooltip title="Add transition">
+                        <ListItemIcon>
+                          <EditSharpIcon />
+                        </ListItemIcon>
+                      </Tooltip>
+                    </ListItem>
+                  </Grid>
+                  <Grid item>
+                    <ListItem
+                      button
+                      selected={selectedIndex === 2}
+                      onClick={(event) => handleListItemClick(event, 2)}
+                    >
+                      <Tooltip title="Mouse">
+                        <ListItemIcon>
+                          <EditSharpIcon />
+                        </ListItemIcon>
+                      </Tooltip>
+                    </ListItem>
+                  </Grid>
+                  <Grid item>
+                    <ListItem
+                      button
+                      selected={selectedIndex === 3}
+                      onClick={(event) => handleListItemClick(event, 3)}
+                    >
+                      <Tooltip title="Undo">
+                        <ListItemIcon>
+                          <EditSharpIcon />
+                        </ListItemIcon>
+                      </Tooltip>
+                    </ListItem>
+                  </Grid>
+                </Grid>
+              </Slide>
+            </Grid>
+            {/* Automata canvas grid */}
+            <Grid item sm={12} md={10}>
+              Automata goes here ...
+            </Grid>
           </Grid>
         </Grid>
       </Box>
